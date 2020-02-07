8f9351c6f04183aae37f4030adb4ebe50bdf16d1
Aylor Brown <aylorbrown@gmail.com>
2020-02-07 14:42:13 -0500

    finishes first exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+const links = [
+  '1', 
+  '2',
+  '3',
+  '4',
+  '5'
+]
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <h1>This is a header!</h1>
+        <ul>
+        {links.map((text, i) => (<li key={i}><Link to={`/${i + 1}`}>{text}</Link></li>))}
+
+
+        </ul>
+    
+
+    <Switch>
+      <Route path="/1">
+        <Home />
+      </Route>
+
+      <Route path="/2">
+        <About />
+      </Route>
+
+      <Route path="/3">
+        <Users />
+      </Route>
+
+      <Route path="/4">
+        <Blog />
+      </Route>
+
+      <Route path="/5">
+        <Work />
+      </Route>
+
+    </Switch>
     </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
+}
+
+function Blog() {
+  return <h2>Blog</h2>
+}
+
+function Work() {
+  return <h2>Work</h2>
+}
+
+
