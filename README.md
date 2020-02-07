c481c7d22fcb4f01c881028ce0ba7d572fa84962
Aylor Brown <aylorbrown@gmail.com>
2020-02-07 15:10:27 -0500

    updates readme for first problem

--- README.md
+++ README.md
@@ -12,3 +12,79 @@ Create a simple React application that routes between 5 components.
 Create a React application that has a single route
 - the route should have a route parameter
 - the component should show the value of the route parameter in an H1
+
+![Router Demo](images/Kapture-2020-02-07-at-15.08.37.gif)
+
+
+Feature #1 
+
+Wraps all the JSX in App with a ```<Router>```, adds a link component to tell Router to activate the route
+
+```javascript
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Welcome to the React Router Demo!</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/work">Work</Link></li>
+        </ul>
+    
+
+    <Switch>
+
+      <Route path="/about">
+        <About />
+      </Route>
+
+      <Route path="/users">
+        <Users />
+      </Route>
+
+      <Route path="/blog">
+        <Blog />
+      </Route>
+
+      <Route path="/work">
+        <Work />
+      </Route>
+
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+    </div>
+    </Router>
+  );
+}
+```
+
+Creates Home, About, etc components that simply return the name of the page 
+
+```javascript 
+function Home() {
+    return <h2>Home</h2>
+  }
+  
+  function About() {
+    return <h2>About</h2>
+  }
+  
+  function Users() {
+    return <h2>Users</h2>
+  }
+  
+  function Blog() {
+    return <h2>Blog</h2>
+  }
+  
+  function Work() {
+    return <h2>Work</h2>
+  }
+}
+```
+
--- src/App.js
+++ src/App.js
@@ -7,71 +7,77 @@ import {
   Link
 } from "react-router-dom";
 
-const links = [
-  '1', 
-  '2',
-  '3',
-  '4',
-  '5'
-]
 
 export default function App() {
   return (
     <Router>
       <div>
-        <h1>This is a header!</h1>
+        <h1>Welcome to the React Router Demo!</h1>
         <ul>
-        {links.map((text, i) => (<li key={i}><Link to={`/${i + 1}`}>{text}</Link></li>))}
-
-
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/work">Work</Link></li>
         </ul>
     
 
     <Switch>
-      <Route path="/1">
-        <Home />
-      </Route>
 
-      <Route path="/2">
+      <Route path="/about">
         <About />
       </Route>
 
-      <Route path="/3">
+      <Route path="/users">
         <Users />
       </Route>
 
-      <Route path="/4">
+      <Route path="/blog">
         <Blog />
       </Route>
 
-      <Route path="/5">
+      <Route path="/work">
         <Work />
       </Route>
 
+      <Route path="/">
+        <Home />
+      </Route>
     </Switch>
     </div>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>
-}
 
-function About() {
-  return <h2>About</h2>
-}
-
-function Users() {
-  return <h2>Users</h2>
-}
-
-function Blog() {
-  return <h2>Blog</h2>
-}
-
-function Work() {
-  return <h2>Work</h2>
-}
+  function Home() {
+    return (
+      <h2>Home</h2>
+    )
+  }
+  
+  function About() {
+    return (
+      <h2>About</h2>
+    )
+  }
+  
+  function Users() {
+    return (
+      <h2>Users</h2>
+    )
+  }
+  
+  function Blog() {
+    return (
+      <h2>Blog</h2>
+    )
+  }
+  
+  function Work() {
+    return (
+      <h2>Work</h2>
+    ) 
+  }
 
 
